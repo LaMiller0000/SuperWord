34eb09693f6d2dfe54a378478fdceb0aa25a6290
LaMiller0000 <160054780+LaMiller0000@users.noreply.github.com>
2024-07-11 01:48:56 -0400

    typing test foundation

---
+++ TypingTest/TypingTest/tTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypingTest
+{
+    internal class tTest
+    {
+        private Timer timer;
+        private string[] test;
+        
+        public tTest() { }
+
+        public tTest(string[] test) { test = this.test; }
+
+        public tTest(string[] test, Timer timer) { test = this.test; timer = this.timer; }
+
+        public float testAvg()
+        {
+            return (0);
+        }
+    }
+}
